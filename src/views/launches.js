e37d569855407bd56b0131a68f02cae537a5dda2
laurion <klauriondareich@gmail.com>
2022-03-16 19:45:43 +0100

    a foreground image added in launches page

--- src/views/launches.js
+++ src/views/launches.js
@@ -1,6 +1,9 @@
 
 const Launches = () => {
     return <div className="main-container">
+
+        <img src="assets/img/nasa.jpg" height="400" width="900" className="nasa-img" alt="nasa-img"/>
+
         <h2>Prochain le lancement</h2>
         <div className="content-element width-500 first">
             <img src="assets/img/rocket-big.png" className="rocket-big-img" alt="rocket big"/>
@@ -9,6 +12,11 @@ const Launches = () => {
         </div>
 
         <h2>Lancements passés</h2>
+        <div className="content-element width-400">
+            <img src="assets/img/rocket.png" alt="rocket"/>
+            <p>Date du lancement : </p>
+            <p><a href="/">Lanceur : </a></p>
+        </div>
         <div className="content-element width-400">
             <img src="assets/img/rocket.png" alt="rocket"/>
             <p>Date du lancement : </p>
