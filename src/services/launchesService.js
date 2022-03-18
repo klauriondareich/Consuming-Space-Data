61d5abd6dd228448afd5ef97d568d1d2dd4db871
laurion <klauriondareich@gmail.com>
2022-03-18 18:26:51 +0100

    a service to handle http requests added

---
+++ src/services/launchesService.js
@@ -0,0 +1,10 @@
+// import axios from "axios";
+
+// let base_path = "https://api.spacex.land";
+// let next_launch_url = base_path + "/rest/launch-next";
+
+// const getNextLaunches = () =>{
+//     return axios.get(next_launch_url)
+// };
+
+// export default getNextLaunches;
