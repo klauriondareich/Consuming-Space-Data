72efa582c645ef530a214cae1405a032425204d6
laurion <klauriondareich@gmail.com>
2022-03-24 12:00:42 +0100

    navbar component injected in App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/home";
 import Launches from "./views/launches";
 import ViewLaunch from "./views/viewLaunch";
+import Navbar from "./views/navbar"
 
 function App() {
   return (  
 
     <BrowserRouter>
+      <Navbar/>
       <Routes>
           <Route index element={<Home />} />
           <Route path="launches" element={<Launches />} />
