0969ec2ed1204cc25ebff0c175ac6b4560bfcc5e
laurion <klauriondareich@gmail.com>
2022-03-26 21:00:11 +0100

    all launches route added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/home";
 import Launches from "./views/launches";
 import ViewLaunch from "./views/viewLaunch";
+import AllLaunches from "./views/allLaunches";
 import Navbar from "./views/navbar"
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="launches" element={<Launches />} />
           <Route path="launches/:id" element={<ViewLaunch />} />
+          <Route path="all_launches" element={<AllLaunches />} />
       </Routes>
     </BrowserRouter>
    
